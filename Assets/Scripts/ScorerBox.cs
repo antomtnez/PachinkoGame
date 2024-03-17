a6d7de75f030bf24354ef5e5a1aa998ebf048dd7
Anto Martínez <antomartinezdev@gmail.com>
2024-03-17 12:06:03 +0100

    I included some vfx and sfx to the game for scoring, buttons click and ambient music

--- Assets/Scripts/ScorerBox.cs
+++ Assets/Scripts/ScorerBox.cs
@@ -4,12 +4,21 @@ public class ScorerBox : MonoBehaviour
 {
     [SerializeField] int points;
     private GameManager gameManager;
+    private BallsSpawner ballsSpawner;
+    private AudioSource scoreAudio;
+    [SerializeField] ParticleSystem scoreEffect;
 
     void Start(){
-        gameManager = FindObjectOfType<GameManager>();
+        GameObject gameManagerGO = GameObject.Find("GameManager");
+        gameManager = gameManagerGO.GetComponent<GameManager>();
+        ballsSpawner = gameManagerGO.GetComponent<BallsSpawner>();
+        scoreAudio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other) {
         gameManager.AddPoints(points);
+        ballsSpawner.BallScored();
+        scoreEffect.Play();
+        scoreAudio.Play();
     }
 }
